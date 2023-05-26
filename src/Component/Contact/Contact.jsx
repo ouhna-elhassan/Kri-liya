bce6cfdaa94f2210511a617d19ae7187e521ccdc
ouhna-elhassan <ouhnahassan01@gmail.com>
2023-05-26 23:16:56 +0100

    first commit

---
+++ src/Component/Contact/Contact.jsx
@@ -0,0 +1,60 @@
+import background from "../../image/header4.jpg"
+export default function Contact() {
+  return (
+    <div className="contact py-5">
+      <div className="page-header" style={{ backgroundImage: `url(${background})` }}>
+        <h3>#Our Contact</h3>
+        <p>We love to hear from you !</p>
+      </div>
+      <div className="container my-5 pt-5">
+        <div className="row g-4">
+          <div className="col-md-7 col-sm-12">
+            <form>
+              <div className="my-4">
+                <textarea cols="30" rows="10" placeholder="Enter your message" className=" form-control rounded-0"></textarea>
+              </div>
+              <div className="my-4 d-flex justify-content-center align-items-center">
+                <input type="text" className=" form-control rounded-0 me-2 py-3" placeholder="Enter your name" />
+                <input type="email" className=" form-control rounded-0 ms-3 py-3" placeholder="Enter Email" />
+              </div>
+              <div className="my-4">
+                <input type="text" className=" form-control rounded-0 py-3" placeholder="Enter your subject" />
+              </div>
+              <button className="btn rounded-0 py-3 px-5">Send</button>
+            </form>
+          </div>
+          <div className="col-md-4 col-sm-12 offset-md-1">
+            <div className="d-flex align-items-center my-5">
+              <i className="fa-solid fa-house fs-2 me-3"></i>
+              <div className="text">
+                <h5 className="m-0 fw-bold">Rabt , Morocco.</h5>
+                <p className="text-muted m-0">Hay al Irfan, CA 91770</p>
+              </div>
+            </div>
+            <div className="d-flex align-items-center my-5">
+              <i className="fa-solid fa-square-phone fs-2 me-3"></i>
+              <div className="text ">
+                <h5 className="m-0 fw-bold">+20123456789</h5>
+                <p className="text-muted m-0">Mon to Fri 9am to 6pm</p>
+              </div>
+            </div>
+            <div className="d-flex align-items-center my-5">
+              <i className="fa-solid fa-envelope fs-2 me-3"></i>
+              <div className="text ">
+                <h5 className="m-0 fw-bold">kriliya@gmail.com</h5>
+                <p className="text-muted m-0">Send us your query anytime!</p>
+              </div>
+            </div>
+            <div className="d-flex align-items-center my-5">
+              <i className="fa-solid fa-globe fs-2 me-3"></i>
+              <div className="text">
+                <h5 className="m-0 fw-bold">Kri_Liya</h5>
+                <p className="text-muted m-0">About us</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
